a163ecdb30ebb5d1cce02469ab594108e3406cca
Wilker Ribeiro <rekliw97@gmail.com>
2021-02-28 02:36:25 -0300

    feat: emit command via http call

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { io } from 'socket.io-client';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    getHello(): string {
+        return 'Hello World!';
+    }
+
+    emitCommand(command: object): void {
+        const wsClient = io('https://clovis-gateway.herokuapp.com/', {
+            reconnectionDelayMax: 10000,
+        }).connect();
+
+        wsClient.emit('event', command);
+
+        wsClient.disconnect();
+    }
 }
